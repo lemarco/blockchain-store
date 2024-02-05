d30334220448894672219ccde1f84bb45b8a3f90
Le Marc <lemarc.dev@gmail.com>
2024-02-05 12:41:39 +0000

    connect metamask to hardhat

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import "dotenv/config";
 const config: HardhatUserConfig = {
-	solidity: "0.8.19",
+	networks: {
+		hardhat: {
+			accounts: {
+				mnemonic: process.env.SEED_PHRASE,
+			},
+			chainId: 1337,
+		},
+	},
 };
-
+console.log("config = ", config.networks?.hardhat);
 export default config;
