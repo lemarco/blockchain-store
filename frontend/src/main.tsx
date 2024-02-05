fbb6ed05173e754f5b3781c60b34822fd679b175
Le Marc <lemarc.dev@gmail.com>
2024-02-05 14:05:00 +0000

    add metamask provider. create connection. login with metamask

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
+import { MetaMaskUIProvider } from "@metamask/sdk-react-ui";
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
+
+const sdkOptions = {
+	dappMetadata: {
+		name: "Example React Dapp",
+		url: window.location.href,
+	},
+};
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<MetaMaskUIProvider debug={true} sdkOptions={sdkOptions}>
+			<App />
+		</MetaMaskUIProvider>
 	</React.StrictMode>,
 );
